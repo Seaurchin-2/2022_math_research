19d589c9d47b4ad59c3196f1f50d5323763a577c
Seaurchin*2 <psj22803540@gmail.com>
2022-06-19 23:10:56 +0900

    Final code
    Troubleshooting index problem(segmanent fault)

--- ExponentialSearch.c
+++ ExponentialSearch.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
-// #include <unistd.h>
+//#include <unistd.h>
 
 int binary_search(int arr[], int start, int end, int value)
 {
-  printf("f=%d b=%d\n", start, end);
     int front=start, back=end;
     int mid = (front + back) / 2;
-
-    printf("loop2 start , mid=%d\n", mid);
     
     while (arr[mid] != value)
     {
-        printf("%d %d\n", front, back);
-        //sleep(1);
         if (front == back)
           return (arr[back] == value) ? back : -1;
 
         if (arr[mid] < value)
-        {
-            front = mid;
-            mid = (front+back) / 2;
-        }
+            front = mid+1;
         else
-        {
-            back = mid;
-            mid = (front+back) / 2;
-        }
+            back = mid-1;
+        mid = (front+back) / 2;
     }
 
     return mid;
@@ -37,27 +27,18 @@ int exponential_search(int arr[], int size, int value)
 
     if (arr[0] == value) return 0;
     else if (arr[size-1] < value) return -1;
-    
-    printf("loop start\n");
 
     while (arr[(xpt<<cnt)] < value)
-    {
-        printf("xpt cal %d -> %d\n", cnt, xpt<<cnt);
-        start = xpt<<(cnt-1);
-        end = xpt<<cnt;
+    {   
         cnt++;
         if ((xpt<<cnt) >= size)
-        {
-            printf("deducted\n");
-            //cnt-=1;
             break;
-        }
-        //sleep(1);
     }
-
-    printf("goto binsearch\n");
+  
+    start = ((xpt<<(cnt-1)) < 0) ? 0 : xpt<<(cnt-1);
+    end = ((xpt<<cnt) >= size) ? size : xpt<<cnt;
     
-    index = binary_search(arr, (xpt<<(cnt-1)), ((xpt<<cnt)>=size)?(size-1):(xpt<<cnt), value);
+    index = binary_search(arr, start, end, value);
 
     return index;
 }
@@ -77,12 +58,8 @@ int main(void)
     // get a value to search
     scanf("%d", &value);
     
-    printf("input end\n");
-    
     // find the value to find by Exponential Search
     index = exponential_search(nums, N, value);
-    
-    printf("search end\n");
 
     // Show the position of the value
     if (index != -1)
